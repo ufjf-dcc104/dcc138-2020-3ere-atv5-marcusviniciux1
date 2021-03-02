2e6807358ffb4a9c1c544deef0a932a9c1d4bd36
Marcus Vinícius Rodrigues da Silva <marcusvinicius.rodrigues@estudante.ufjf.br>
2021-03-02 16:42:32 -0300

    Adiciona carregamento de áudio

--- js/Main.js
+++ js/Main.js
@@ -7,6 +7,7 @@ const assets = new AssetManager();
 assets.carregaImagem("garota", "assets/garota.png");
 assets.carregaImagem("orc", "assets/orc.png");
 assets.carregaImagem("esqueleto", "assets/skelly.png");
+assets.carregaAudio("moeda", "assets/sound.wav");
 
 const canvas = document.querySelector("canvas");
 const cena1 = new Cena(canvas, assets);
